6da09f30b3ba6bf8cd91fbf82bf40619b32a0fd5
Muchiri <developedbyrobbie@gmail.com>
2024-10-21 15:44:13 +0300

    layout

--- src/components/elements/header.tsx
+++ src/components/elements/header.tsx
@@ -2,8 +2,10 @@ import SidebarSheet from "./sidebar-sheet"
 
 const Header = () => {
   return (
-    <div className="py-5">
-      <SidebarSheet />
+    <div className="py-5 flex items-center gap-2">
+      <div className="lg:hidden">
+        <SidebarSheet />
+      </div>
       Header
     </div>
   )
